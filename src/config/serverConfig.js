4f8b8aae143724c2cec868f860e610005e13e112
PRANAV8966 <pranav.wan706@gmail.com>
2025-03-06 16:49:17 +0530

    removed sensitive information

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,4 +3,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const MONGO_SERVER_URL = process.env.MONGO_SERVER_URL;
-export const JWT_KEY = process.env.JWT_KEY;
+// export const JWT_KEY = process.env.JWT_KEY;
+// export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+// export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
